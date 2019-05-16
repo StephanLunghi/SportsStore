99c10d38430e23a31514dfb8779e6ff4acc52359
Stephan Lunghi <slunghi43@gmail.com>
2019-05-16 12:20:23 -0400

    Created a unit test to confirm creation of categories

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
-        
-        public string Menu()
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo)
         {
-            return "Hello from NavController";
+            repository = repo;
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
         }
     }
 }
