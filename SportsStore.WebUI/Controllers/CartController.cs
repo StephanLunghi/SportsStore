61fd48cb7e0fcc78781814db57def322e95146bd
Stephan Lunghi <slunghi43@gmail.com>
2019-05-17 09:32:16 -0400

    Added the Index action method in the CartController

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -16,6 +17,14 @@ namespace SportsStore.WebUI.Controllers
         {
             repository = repo;
         }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel { Cart = GetCart(), ReturnUrl = returnUrl });
+        }
+
+
+
 
         public RedirectToRouteResult AddToCart(int productId, string returnUrl)
         {
